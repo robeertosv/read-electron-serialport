c399e875dbb619f0e22a8e85b3da68032c471207
Rober <121724164+robeertosv@users.noreply.github.com>
2023-01-07 23:08:44 +0100

    IMPORTANT COMMENT ADDED
    Modify path and baudrate to fit your project

--- src/views/script.js
+++ src/views/script.js
@@ -2,8 +2,8 @@ const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
 
 const port = new SerialPort({
-    path: 'COM5',
-    baudRate: 9600
+    path: 'COM5', //EDIT AS NEEDED
+    baudRate: 9600 //EDIT AS NEEDED
 })
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
